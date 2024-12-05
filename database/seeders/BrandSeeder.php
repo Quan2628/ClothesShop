c6449af230565d5e71056efa0460b635ea051a9f
Quan2628 <builongquan2601@gmail.com>
2024-12-05 10:31:42 +0700

    pj2

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BrandSeeder extends Seeder
 {
@@ -13,5 +14,12 @@ class BrandSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('brand_product')->insert([
+            [
+                'brand_name'=>'TeeLab',
+                'brand_description'=>'Trẻ trung, năng động',
+                'brand_status'=>1
+            ]
+        ]);
     }
 }
